90b3f6027d13acd7af5df80c2b3aff0d7bb9ba0e
Sandra Moreno Palacios <sanmorpa@gmail.com>
2022-12-04 12:56:24 +0100

    modified script: asks permission to pip install

--- folder_creator.sh
+++ folder_creator.sh
@@ -4,13 +4,30 @@
 
 PURPLE='\033[0;35m'
 YELLOW='\033[0;33m'
+RED='\033[0;31m'
 NC='\033[0m' # No Color
 
 python3 -c "import aocd"
 
 if [ $(echo $?) -gt 0 ]
 then
-	pip install advent-of-code-data
+
+	printf "${YELLOW}WARNING. You don't have advent-of-code-data python package installed. ${NC}\n"
+	printf "Would you like to install it now? (Y/N)\n>> "
+	read ANSWER
+	while [ "$ANSWER" != "Y" ] && [ "$ANSWER" != "y" ] && [ "$ANSWER" != "N" ] && [ "$ANSWER" != "n" ]
+	do
+	printf "Answer must be Y (Yes) or N (No)\n>> "
+		read ANSWER
+	done
+	if [ "$ANSWER" == "Y" ] || [ "$ANSWER" == "y" ]
+	then
+		pip install advent-of-code-data
+	elif [ "$ANSWER" == "N" ] || [ "$ANSWER" == "n" ]
+	then
+		printf "${RED}ERROR. Unable to proceed because 'advent-of-code-data' python packaged is not installed ${NC}"
+		exit 0
+	fi
 fi
 
 if [[ -z "${AOC_SESSION}" ]]
