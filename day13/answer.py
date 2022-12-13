0d497fc40abc1afee7506b67b6931a7fc8c5b1ce
Sandra Moreno Palacios <sanmorpa@gmail.com>
2022-12-13 14:13:40 +0100

    Doesn't work but it's closer

--- day13/answer.py
+++ day13/answer.py
@@ -19,39 +19,52 @@ def parse_to_list(s, i=0):
 
 # Data ingestion and processing
 with open('input.txt') as file:
-	lines = [line.rstrip().replace(',', ' ') for line in file]
-
-lines = [parse_to_list(line) for line in lines]
+	lines = [line.rstrip() for line in file]
+lines = [eval(line) for line in lines if len(line) > 0]
 
 signals = list()
 new_line = list()
+i = 0
 for line in lines:
-	if len(line) == 0:
+	if i == 2:
 		signals.append(new_line)
 		new_line = list()
-	else:
-		new_line.append(line)
+		i = 0
+	new_line.append(line)
+	i += 1
 signals.append(new_line)
 
-def check_values(s1, s2, pair):
+#for line in signals:
+#	print(line[0])
+#	print(line[1])
+#	print('     --------         ')
+
+def check_values(s1, s2):
+	print(f"S1: {s1}\nS2: {s2}\n")
 	for signal1, signal2 in zip_longest(s1, s2):
+		print(f"Signal1: {signal1}\nSignal2: {signal2}\n")
 		if signal1 == None and signal2 != None:
 			return True
 		elif signal2 == None and signal1 != None:
 			return False
 		if isinstance(signal1, list):
 			if isinstance(signal2, list):
-				if (len(signal1) == 0 and len(signal2) != 0):
-					return True
-				elif (len(signal2) == 0 and len(signal1) != 0):
-					return False
-				elif (len(signal1) != 0 and len(signal2) != 0):
-					return check_values(signal1, signal2, pair)
+				if signal1 != signal2:
+					if (len(signal1) == 0 and len(signal2) != 0):
+						return True
+					elif (len(signal2) == 0 and len(signal1) != 0):
+						return False
+					elif (len(signal1) != 0 and len(signal2) != 0):
+						return check_values(signal1, signal2)
 			elif isinstance(signal2, str):
-				return check_values(signal1, list(signal2), pair)
+				lst_signal = list(signal2)
+				if lst_signal != signal1:
+					return check_values(signal1, list(signal2))
 		elif isinstance(signal1, str):
 			if isinstance(signal2, list):
-				return check_values(list(signal1), signal2, pair)
+				lst_signal = list(signal1)
+				if lst_signal != signal2:
+					return check_values(list(signal1), signal2)
 			elif isinstance(signal2, str):
 				if int(signal1) < int(signal2):
 					return True
@@ -62,6 +75,10 @@ total = 0
 for pair in range(len(signals)):
 	pair1 = signals[pair][0]
 	pair2 = signals[pair][1]
-	if check_values(pair1, pair2, pair) == True:
+	if check_values(pair1, pair2) == True:
+		print("True")
 		total += (pair + 1)
+	else:
+		print("False")
+	print()
 print(f"The total is {total}")
